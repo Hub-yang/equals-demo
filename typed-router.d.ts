3929e1176c84239d69059ea2f4bc316d7750c94d
Hub-yang <18830279823@163.com>
2024-02-25 21:14:56 +0800

    update: change options

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,7 +40,6 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/test': RouteRecordInfo<'/test', '/test', Record<never, never>, Record<never, never>>,
   }
 }
 
