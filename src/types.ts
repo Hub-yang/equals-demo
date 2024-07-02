520b086339ab50e07f5dde9bc5f99acb76958d43
HuberyYang <78254474+Hub-yang@users.noreply.github.com>
2024-07-02 09:53:07 +0800

    fix: 为Value增加扩展类型

--- src/types.ts
+++ src/types.ts
@@ -3,24 +3,9 @@ export interface BlockState {
   y: number
 }
 
-export type Value = (true
-  | false
-  | 1
-  | 0
-  | -1
-  | 'true'
-  | 'false'
-  | '1'
-  | '0'
-  | '-1'
-  | ''
-  | null
-  | undefined
-  | typeof Number.POSITIVE_INFINITY
-  | typeof Number.NEGATIVE_INFINITY
-  | []
-  | [0]
-  | [1]
-  | [[]]
-  | object
-  | typeof Number.NaN)
+type PrimitiveValues = true | false | 1 | 0 | -1 | 2 | 'true' | 'false' | '1' | '0' | '-1' | '2' | ''
+type SpecialValues = null | undefined | typeof Number.POSITIVE_INFINITY | typeof Number.NEGATIVE_INFINITY | typeof Number.NaN
+type ArrayValues = [] | [0] | [1] | [[]]
+type ObjectValues = object
+
+export type Value = PrimitiveValues | SpecialValues | ArrayValues | ObjectValues
