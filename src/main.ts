402c90ab651b52220250e18338591f3c83dda4c0
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2025-06-07 23:16:30 +0800

    feat: update deps

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 
 import 'uno.css'
@@ -7,6 +8,7 @@ import '@unocss/reset/tailwind.css'
 
 const app = createApp(App)
 const router = createRouter({
+  routes,
   history: createWebHistory(import.meta.env.BASE_URL),
 })
 app.use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import VueRouter from 'unplugin-vue-router/vite'
+import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
-import UnoCSS from 'unocss/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+import { defineConfig } from 'vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
